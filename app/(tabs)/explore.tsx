aeb7e533a84e0b214c269fcd1b9fa9ee112fafbb
TzuyuSkywalker <deciimeegaabb@gmail.com>
2026-03-27 15:49:36 -0300

    v1

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,98 +1,144 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { Collapsible } from '@/components/ui/collapsible';
-import { ExternalLink } from '@/components/external-link';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Fonts } from '@/constants/theme';
+import { ExternalLink } from "@/components/external-link";
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { Collapsible } from "@/components/ui/collapsible";
+import { Fonts } from "@/constants/theme";
+import { Image, StyleSheet } from "react-native";
 
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#000000ff" }}
       headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
+        <Image
+          source={require("../../assets/images/deadline.png")}
           style={styles.headerImage}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
         <ThemedText
           type="title"
           style={{
             fontFamily: Fonts.rounded,
-          }}>
-          Explore
+          }}
+        >
+          ¿Quieres ser BLINK? Te presento a BLACKPINK
         </ThemedText>
       </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
+      <Collapsible title="Conocé a Rosé">
         <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
+          Rosé es una cantante y compositora reconocida mundialmente por ser la
+          voz principal de BLACKPINK.
         </ThemedText>
         <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
+          Nació el 11 de febrero de 1997 en Auckland, Nueva Zelanda, y creció en
+          Australia antes de mudarse a Corea del Sur para perseguir su sueño en
+          la música.
         </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
+        <ThemedText>
+          Debutó en 2016 con BLACKPINK y se destacó por su voz única y
+          emocional, convirtiéndose en una de las artistas más influyentes del
+          K-pop.
+        </ThemedText>
+        <ThemedText>
+          En 2021 lanzó su carrera solista con el álbum “R”, mostrando un estilo
+          más personal y consolidando su éxito a nivel global.
+        </ThemedText>
+        <ExternalLink href="https://www.instagram.com/roses_are_rosie/">
+          <ThemedText type="link">Instagram de Rosé</ThemedText>
         </ExternalLink>
       </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
+      <Collapsible title="Conocé a Jennie">
         <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
+          Jennie es rapera, cantante y una de las integrantes más versátiles de
+          BLACKPINK.
         </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
         <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
+          Nació el 16 de enero de 1996 en Corea del Sur y vivió varios años en
+          Nueva Zelanda, lo que le permitió dominar el inglés.
         </ThemedText>
-        <Image
-          source={require('@/assets/images/react-logo.png')}
-          style={{ width: 100, height: 100, alignSelf: 'center' }}
-        />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
+        <ThemedText>
+          Debutó en 2016 con BLACKPINK y rápidamente se destacó por su presencia
+          escénica y estilo único.
+        </ThemedText>
+        <ThemedText>
+          En 2018 lanzó su canción solista “SOLO”, convirtiéndose en un gran
+          éxito internacional.
+        </ThemedText>
+        <ExternalLink href="https://www.instagram.com/jennierubyjane/">
+          <ThemedText type="link">Instagram de Jennie</ThemedText>
         </ExternalLink>
       </Collapsible>
-      <Collapsible title="Light and dark mode components">
+      <Collapsible title="Conocé a Lisa">
         <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user&apos;s current color scheme is, and so you can adjust UI colors accordingly.
+          Lisa es rapera, bailarina principal y una de las idols más populares
+          del K-pop a nivel mundial.
         </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
+        <ThemedText>
+          Nació el 27 de marzo de 1997 en Tailandia y es la única integrante
+          extranjera de BLACKPINK.
+        </ThemedText>
+        <ThemedText>
+          Es reconocida por su increíble habilidad para el baile y su carisma en
+          el escenario.
+        </ThemedText>
+        <ThemedText>
+          En 2021 debutó como solista con “LALISA”, rompiendo récords en YouTube
+          y consolidando su éxito global.
+        </ThemedText>
+        <ExternalLink href="https://www.instagram.com/lalalalisa_m/">
+          <ThemedText type="link">Instagram de Lisa</ThemedText>
         </ExternalLink>
       </Collapsible>
-      <Collapsible title="Animations">
+      <Collapsible title="Conocé a Jisoo">
         <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful{' '}
-          <ThemedText type="defaultSemiBold" style={{ fontFamily: Fonts.mono }}>
-            react-native-reanimated
-          </ThemedText>{' '}
-          library to create a waving hand animation.
+          Jisoo es cantante y visual de BLACKPINK, conocida por su elegancia y
+          carisma.
         </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
+        <ThemedText>
+          Nació el 3 de enero de 1995 en Corea del Sur y es la integrante mayor
+          del grupo.
+        </ThemedText>
+        <ThemedText>
+          Además de la música, ha participado como actriz en dramas,
+          destacándose en “Snowdrop”.
+        </ThemedText>
+        <ThemedText>
+          En 2023 debutó como solista con “FLOWER”, logrando gran éxito en todo
+          el mundo.
+        </ThemedText>
+        <ExternalLink href="https://www.instagram.com/sooyaaa__/">
+          <ThemedText type="link">Instagram de Jisoo</ThemedText>
+        </ExternalLink>
+      </Collapsible>
+      <Collapsible title="Membresía BLINK">
+        <ThemedText>
+          La BLINK MEMBERSHIP es la membresía oficial de BLACKPINK en Weverse,
+          que permite a los fans acceder a beneficios exclusivos dentro de la
+          comunidad.
+        </ThemedText>
+        <ThemedText>
+          Tiene una duración de 365 días desde la compra y está disponible
+          durante todo el año para cualquier usuario con cuenta en Weverse.
+        </ThemedText>
+        <ThemedText>
+          Entre sus beneficios se incluyen acceso a contenido exclusivo,
+          oportunidades especiales para eventos como conciertos y preventas, y
+          una tarjeta digital de membresía.
+        </ThemedText>
+        <ThemedText>
+          También existe la opción de comprar un “Membership Kit” por separado,
+          que incluye productos físicos oficiales (hasta agotar stock).
+        </ThemedText>
+        <ThemedText>
+          Es importante usar la misma cuenta en Weverse y Weverse Shop para
+          poder acceder correctamente a todos los beneficios.
+        </ThemedText>
+        <ExternalLink href="https://shop.weverse.io/es/shop/USD/artists/32/sales/51519">
+          <ThemedText type="link">Adquiérela aquí</ThemedText>
+        </ExternalLink>
       </Collapsible>
     </ParallaxScrollView>
   );
@@ -100,13 +146,15 @@ export default function TabTwoScreen() {
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+    height: 120,
+    width: 375,
+    bottom: -35,
+    position: "relative",
+    alignSelf: "center",
+    marginTop: 32,
   },
   titleContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 8,
   },
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,76 +1,88 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Image } from "expo-image";
+import { StyleSheet } from "react-native";
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { Link } from "expo-router";
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#d42cb0ff", dark: "#000000ff" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/bplogo.png")}
           style={styles.reactLogo}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">
+          Guia como stannear al BEST GG del mundo
+        </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">
+          1. Conocé a las integrantes y su rol
+        </ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
+          {""}
           <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
+            Para ser un buen fan tenés que ubicar bien a cada miembro: {"\n"}
+            Jennie – rapera principal y vocalista {"\n"}
+            Lisa – bailarina principal y rapera{"\n"}
+            Jisoo – vocalista principal y visual {"\n"}
+            Rosé – vocalista principal
+          </ThemedText>{" "}
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <Link href="/modal">
           <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+            <ThemedText type="subtitle">
+              2. Apoyá siempre sus comebacks y contenido
+            </ThemedText>
           </Link.Trigger>
           <Link.Preview />
           <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
+            <Link.MenuAction
+              title="Action"
+              icon="cube"
+              onPress={() => alert("Action pressed")}
+            />
             <Link.MenuAction
               title="Share"
               icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
+              onPress={() => alert("Share pressed")}
             />
             <Link.Menu title="More" icon="ellipsis">
               <Link.MenuAction
                 title="Delete"
                 icon="trash"
                 destructive
-                onPress={() => alert('Delete pressed')}
+                onPress={() => alert("Delete pressed")}
               />
             </Link.Menu>
           </Link.Menu>
         </Link>
 
         <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
+          {`El fandom se llama BLINK 💕\nNo generar peleas innecesarias\nDefender con respeto\nApoyar a las chicas individualmente y como grupo\n👉 Regla: fan fiel pero sin toxicidad.`}{" "}
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+        <ThemedText type="subtitle">
+          3. Respetá al fandom y evitá hate
+        </ThemedText>
         <ThemedText>
           {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+          <ThemedText type="defaultSemiBold">
+            npm run reset-project
+          </ThemedText>{" "}
+          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
+          directory. This will move the current{" "}
+          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
@@ -80,8 +92,8 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -89,10 +101,11 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+    height: 120,
+    width: 375,
+    bottom: -35,
+    position: "relative",
+    alignSelf: "center",
+    marginTop: 32,
   },
 });
